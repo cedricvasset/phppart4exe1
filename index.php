312a062d6282dda27bcde9c015bef2254228cbd3
cedricvasset <vasset.cedric@orange.fr>
2018-12-21 16:19:47 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8" />
+  <title>php part3 exe1</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+  <?php
+  function returnTrue(){
+    return true;
+  }
+  echo returnTrue();
+  ?>
+</body>
+</html>
